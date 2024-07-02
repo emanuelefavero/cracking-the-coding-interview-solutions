9371d762ef6fa231f5a3774f7619937e4ec75660
emanuelefavero <emafaux89@gmail.com>
2024-07-02 18:41:57 +0200

    add arrays-and-strings/is-unique solution 2

--- arrays-and-strings/is-unique.ts
+++ arrays-and-strings/is-unique.ts
@@ -11,7 +11,29 @@ function isUnique(string: string) {
 }
 
 // --------------------
+// SOLUTION 2
+
+// TIP: This solution does not use additional data structures
+
+function isUnique2(string: string) {
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j < string.length; j++) {
+      if (string[i] === string[j]) {
+        return false
+      }
+    }
+  }
+
+  return true
+}
+
+// --------------------
 // TESTS
 
+// 1
 console.log(isUnique('hello')) // false
 console.log(isUnique('abc')) // true
+
+// 2
+console.log(isUnique2('hello')) // false
+console.log(isUnique2('abc')) // true
