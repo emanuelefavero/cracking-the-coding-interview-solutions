29a5015bd92f657d834dc84a4df440af239b523c
emanuelefavero <emafaux89@gmail.com>
2024-07-03 20:21:17 +0200

    add arrays-and-strings/check-permutation

---
+++ arrays-and-strings/check-permutation.ts
@@ -0,0 +1,13 @@
+// Given two strings, write a method to decide if one is a permutation of the other.
+
+function checkPermutation(str1: string, str2: string) {
+  if (str1.length !== str2.length) return false
+
+  let sorted1 = str1.split('').sort().join('')
+  let sorted2 = str2.split('').sort().join('')
+
+  return sorted1 === sorted2
+}
+
+console.log(checkPermutation('abc', 'bca')) // true
+console.log(checkPermutation('abc', 'bcd')) // false
