216b10917790e2efdfc8e3158ca0cc7315de15cd
emanuelefavero <emafaux89@gmail.com>
2024-07-04 15:20:14 +0200

    add arrays-and-strings/URLify solution 2

--- arrays-and-strings/URLify.ts
+++ arrays-and-strings/URLify.ts
@@ -1,12 +1,22 @@
 // URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the "true" length of the string.
 
+// TIP: This solution uses regular expressions
+
 function replaceSpaces(string: string) {
-  string = string.trim()
-  string = string.replace(/\s/g, '%20')
-  return string
+  return string.trim().replace(/\s/g, '%20')
+}
+
+// --------------------
+// SOLUTION 2
+
+// TIP: This solution uses the built-in encodeURI function that converts a string into a valid URL
+
+function replaceSpaces2(string: string) {
+  return encodeURI(string.trim())
 }
 
 // --------------------
 // TESTS
 
 console.log(replaceSpaces('Mr John Smith   '))
+console.log(replaceSpaces2('Mr John Smith   '))
