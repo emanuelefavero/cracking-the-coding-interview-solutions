11cbf333980ad13ce92813ffb91cca1addd548f3
emanuelefavero <emafaux89@gmail.com>
2024-07-04 15:25:34 +0200

    add arrays-and-strings/URLify solution 4

--- arrays-and-strings/URLify.ts
+++ arrays-and-strings/URLify.ts
@@ -23,8 +23,26 @@ function replaceSpaces3(string: string) {
 }
 
 // --------------------
+// SOLUTION 4
+
+// TIP: This solution uses a for loop to iterate over the string and replace spaces with '%20' in place
+
+function replaceSpaces4(string: string) {
+  string = string.trim()
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
+      string = string.slice(0, i) + '%20' + string.slice(i + 1)
+    }
+  }
+
+  return string
+}
+
+// --------------------
 // TESTS
 
 console.log(replaceSpaces('Mr John Smith   '))
 console.log(replaceSpaces2('Mr John Smith   '))
 console.log(replaceSpaces3('Mr John Smith   '))
+console.log(replaceSpaces4('Mr John Smith   '))
